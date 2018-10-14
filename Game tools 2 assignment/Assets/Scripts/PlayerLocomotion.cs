d1ed9d55e7059485c74fcb3f220a89014ab1524b
Finnchris13 <finnchris13@gmail.com>
2018-10-14 22:22:18 +0100

    Uploading the current state.
    The reason the first upload has a lot in it is because I made a lot of progress early and didn't know how to use GitHub. The game currently has the camera, some self made animations and imported animations, and imported models, aswell as user controller and player movement.

---
+++ Game tools 2 assignment/Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour {
+
+    private Animator m_anim;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        m_anim = GetComponent<Animator>();
+
+	}
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            m_anim.SetBool("isAiming", true);
+        }
+        else
+        {
+            m_anim.SetBool("isAiming", false);
+        }
+    }
+
+    public void Move(float forward, float turn)
+    {
+        m_anim.SetFloat("IsWalking", forward);
+        m_anim.SetFloat("IsTurning", turn);
+    }
+}
---
+++ Game tools 2 assignment/Assets/Scripts/UserControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControls : MonoBehaviour {
+
+    private float m_forward;
+    private float m_turn;
+
+    private PlayerLocomotion m_locomotion;
+
+	// Use this for initialization
+	void Start () {
+        m_locomotion = GetComponent<PlayerLocomotion>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        m_forward = Input.GetAxis("Vertical");
+        m_turn = Input.GetAxis("Horizontal");
+        m_locomotion.Move(m_forward, m_turn);
+	}
+}
